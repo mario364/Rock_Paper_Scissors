f60713d813b6c580cd736208d3a771e09be0cd16
Maxim Savikin <maxsavikin@gmail.com>
2022-10-23 12:13:47 +0300

    final commit

--- rock_paper_Scisor.py
+++ rock_paper_Scisor.py
@@ -3,23 +3,31 @@ import time
 
 machine_answres = ["Камень", "Ножницы", "Бумага"]
 
-def play(player_answer):
-    machine_answres1 = random.choice(machine_answres)
-    print(f'Ответ игрока: {player_answer}')
-    time.sleep(1)
-    print(f'Ответ машины: {machine_answres1}')
-    time.sleep(1)
-    if player_answer == "Камень" and machine_answres1 == 'Ножницы' or player_answer == "Ножницы" and machine_answres1 == 'Бумага'or player_answer == 'Бумага' and machine_answres1 == "Камень":
-        time.sleep(1)
-        print('Ты победил! Дай пять!')
-    elif player_answer == machine_answres1:
-        time.sleep(1)
-        print("Ничья")
-    else:
-        time.sleep(1)
-        print("Ты проиграл! Утешительное дай пять!")
 
+def play():
+    while True:
+        player_answer = input('Камень, Ножницы, Бумага? ')
+        machine_answres1 = random.choice(machine_answres)
+        print(f'Ответ игрока: {player_answer}')
+        time.sleep(1)
+        print(f'Ответ машины: {machine_answres1}')
+        time.sleep(1)
+        if player_answer == "Камень" and machine_answres1 == 'Ножницы' or player_answer == "Ножницы" and machine_answres1 == 'Бумага' or player_answer == 'Бумага' and machine_answres1 == "Камень":
+            time.sleep(1)
+            print('Ты победил! Дай пять!')
+        elif player_answer == machine_answres1:
+            time.sleep(1)
+            print("Ничья")
+        else:
+            time.sleep(1)
+            print("Ты проиграл! Утешительное дай пять!")
+        new_game = int(input('Хочешь сыграть еще раз?\n1 - Еще раз\n2 - Выйти '))
+        if new_game == 1:
+            print("Сыграем еще раз!")
+            continue
+        if new_game == 2:
+            print('Значит в следующий раз! Пока!')
+            break
 
 print('Привет! Давай сыграем в камень, ножницы бумага!')
-player_answer = input('Камень, Ножницы, Бумага? ')
-play(player_answer)
+play()
